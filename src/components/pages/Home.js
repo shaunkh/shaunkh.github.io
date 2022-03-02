c0e4b13efa4a08e21825e33dfcedd1e5e75def90
shaunkh <shaun.khundker@gmail.com>
2022-03-02 16:11:02 +0800

    fix css issues

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,6 +9,9 @@ function Home() {
       <div id="container">
         <div id="writing-container">
           <h1 className="page-title">Hi my name is Shaun.</h1>
+          <h2 className="page-description">
+            Contact me <a className="link-deco" href="https://linktr.ee/shaunkhundker"><b>here</b></a>!
+          </h2>
         </div>
         <div id="my-image">
           <img
@@ -16,11 +19,6 @@ function Home() {
             alt="Shaun Khundker's Headshot"
             src={pfp}
           ></img>
-        </div>
-        <div className="desc-container-no-margin">
-          <h2 className="page-description">
-            Contact me <a className="link-deco" href="https://linktr.ee/shaunkhundker"><b>here</b></a>!
-          </h2>
         </div>
 
       </div>
