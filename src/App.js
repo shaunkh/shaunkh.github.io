416811009dc3286a067547a90f95f808a54266a4
shaunkh <shaun.khundker@gmail.com>
2022-03-03 14:14:03 +0800

    fixed animations, fixed main page design

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import Home from "./components/pages/Home";
 import Work from "./components/pages/Work";
 import Footer from "./components/pages/Footer/Footer";
 import React from "react";
-import Helmet from "react-helmet";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NotFound from "./components/pages/NotFound";
 
 const TITLE = "Shaun Khundker";
 class App extends React.Component {
+  componentDidMount() {
+    document.title = "Shaun's Website"
+  }
   render() {
     return (
       <Router>
-        <Helmet>
-          <title>{TITLE}</title>
-        </Helmet>
+        <title>{TITLE}</title>
         <div className="App">
           <div id="body-format">
             <Navbar></Navbar>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -13,21 +13,27 @@ function Home() {
             src={pfp}
           ></img>
           <div id="about-container">
-            <h1 className="page-title">Hi my name is Shaun.</h1>
-            <h2 className="page-description">Hallo</h2>
+            <h1 className="page-title">Hi, my name is Shaun Khundker.</h1>
+            <div className="title-main">
+            <pre className="page-title">I like to </pre>
+              <div className="title">
+                <div className="title-wrapper">
+                  <div className="title-item">build.</div>
+                  <div className="title-item">research.</div>
+                  <div className="title-item">write.</div>
+                  <div className="title-item">trade.</div>
+                </div>
+              </div>
+            </div>
+            <h2 className="page-description">I spend any free time I get researching
+            DeFi protocols, buying NFTs and building things I find cool. I also like to play
+            guitar and race.</h2>
             <h2 className="page-description">
               Contact me{" "}
               <a className="link-deco" href="https://linktr.ee/shaunkhundker">
                 <b>here</b>
               </a>
             </h2>
-            <div className="title">
-              <div className="title-wrapper">
-                <div className="title-item">Buildoor</div>
-                <div className="title-item">Writoor</div>
-                <div className="title-item">Rotatoor </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
