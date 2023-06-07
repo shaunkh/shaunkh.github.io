94c1b02780c502f923952359945b3365c88b22d0
shaunkh <shaun.khundker@gmail.com>
2023-06-07 11:19:58 -0400

    update 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,23 @@ import { Link } from "react-router-dom";
 import shaunLogo from "../imgs/Shaun Logo-02.png";
 
 function Navbar() {
-  const navStyle = {
+  const linkStyle = {
     color: "white",
     textDecoration: "none",
+    fontFamily: "Barlow Condensed",
+    fontSize: "2rem",
+    margin: "0 10rem",
   };
-
   return (
     <>
       <style>
         @import
         url('https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@100;400&family=Bebas+Neue&display=swap');
       </style>
+
       <nav>
         <Link to="/">
-          <div className="nav-div-p">
+          <div className="nav-div">
             <img
               className="nav-logo"
               alt="Shaun Khundker's Logo"
@@ -25,16 +28,25 @@ function Navbar() {
             ></img>
           </div>
         </Link>
-
-        <div className="nav-div">
-          <ul className="nav-links">
-            <Link style={navStyle} to="/work">
-              <li>Resume</li>
-            </Link>
-            <Link style={navStyle} to={{ pathname: "https://shaunkh.xyz" }} target="_blank">
-            <li>Blog</li>
-            </Link>
-          </ul>
+      </nav>
+      <nav style={{ maxHeight: "40px", marginTop: "-85px" }}>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Link style={linkStyle} to="/work">
+            Work
+          </Link>
+          <Link
+            style={linkStyle}
+            to={{ pathname: "https://shaunkh.xyz" }}
+            target="_blank"
+          >
+            Blog
+          </Link>
         </div>
       </nav>
     </>
